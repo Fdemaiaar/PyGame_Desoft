5c3ac7056b0840ba4137ab76f99091950bd0fe16
Felipe Maia <felipeaffonsormaia@gmail.com>
2022-06-04 12:50:59 -0300

    ibi

--- main.py
+++ main.py
@@ -31,21 +31,31 @@ class Imagem():
         self.arquivo = arquivo
         self.tamanhox = tamanhox
         self.tamanhoy = tamanhoy
-        self.obs = pygame.image.load(self.arquivo).convert_alpha()
-        self.obs = pygame.transform.scale(self.obs,(self.tamanhox,self.tamanhoy))
+        self.img = pygame.image.load(self.arquivo).convert_alpha()
+        self.img = pygame.transform.scale(self.img,(self.tamanhox,self.tamanhoy))
 
     def carregar(self):
-        return self.obs
+        return self.img
     
     def plot(self, x, y):
-        return screen.blit(self.obs,(x,y))
+        return screen.blit(self.img,(x,y))
     
     def retangulo(self):
-        return self.obs.get_rect()
+        return self.img.get_rect()
 
-imagens = [Imagem('imagens/ladrao.png',100,105), Imagem('imagens/policia.png',100,100), Imagem('imagens/cone.png', 67, 67), Imagem('imagens/pedra.png', 67, 67), Imagem('imagens/roda.PNG', 67, 67), Imagem('imagens/1vida.png',17.5,15), Imagem('imagens/2vidas.png',40,15), Imagem('imagens/3vidas.png',62.5,15)]
-for imagem in imagens:
-    imagem.carregar()
+imagens = {
+    'ladrao': Imagem('imagens/ladrao.png',100,105), 
+    'policia': Imagem('imagens/policia.png',100,100),
+    'cone': Imagem('imagens/cone.png', 67, 67),
+    'pedra': Imagem('imagens/pedra.png', 67, 67),
+    'roda': Imagem('imagens/roda.PNG', 67, 67),
+    '1vida': Imagem('imagens/1vida.png',17.5,15),
+    '2vidas': Imagem('imagens/2vidas.png',40,15),
+    '3vidas': Imagem('imagens/3vidas.png',62.5,15)
+}
+
+for imagem in imagens.keys():
+    imagens[imagem].carregar()
 
 
 # Posições iniciais dos Carros
@@ -55,7 +65,7 @@ policia_x = 300
 policia_y = 350
 
 possivel_x = np.arange(110,411,100) # lista de possíveis posições x pro obstáculo
-obstaculos = [imagens[2],imagens[3],imagens[4]]
+obstaculos = [imagens['cone'],imagens['pedra'],imagens['roda']]
 
 # Texto
 font = pygame.font.SysFont(None, 24)
@@ -77,10 +87,11 @@ corquad = (253,196,101) # Cor do Quadrado
 while jogo:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            jogo = False
+            jogo = False # Encerra o jogo quando o usuário sai da tela
 
     screen.blit(bg, (0,0))
-    velocidade = 1
+    velocidade = 2
+    tecla = pygame.key.get_pressed()
 
     # Movimento da Pista
     rel_y = y % bg.get_rect().height
@@ -102,13 +113,13 @@ while jogo:
         obst = False # se o obstaculo passar a tela não existem mais obstáculo no jogo
 
     # Imagens
-    imagens[0].plot(ladrao_x,ladrao_y) # Ladrão
-    imagens[1].plot(policia_x,policia_y) # Policia
+    imagens['ladrao'].plot(ladrao_x,ladrao_y) # Ladrão
+    imagens['policia'].plot(policia_x,policia_y) # Policia
 
 
     # Colisões
-    ladrao_rect = imagens[0].retangulo()
-    policia_rect = imagens[1].retangulo()
+    ladrao_rect = imagens['ladrao'].retangulo()
+    policia_rect = imagens['policia'].retangulo()
     obstaculo_rect = o.retangulo()
 
     ladrao_rect.x = ladrao_x
@@ -134,20 +145,19 @@ while jogo:
     pygame.draw.rect(screen, (0,0,0), pygame.Rect(513,15,88,50),4) # Contorno Retângulo Vida Policia
 
     if batidas_lad == 0:
-        imagens[7].plot(15,40)
+        imagens['3vidas'].plot(15,40)
     elif batidas_lad == 1:
-        imagens[6].plot(15,40)
+        imagens['2vidas'].plot(15,40)
     elif batidas_lad == 2:
-        imagens[5].plot(15,40)
+        imagens['1vida'].plot(15,40)
     if batidas_pol == 0:
-        imagens[7].plot(530,40)
+        imagens['3vidas'].plot(530,40)
     elif batidas_pol == 1:
-        imagens[6].plot(530,40)
+        imagens['2vidas'].plot(530,40)
     elif batidas_pol == 2:
-        imagens[5].plot(530,40)
+        imagens['1vida'].plot(530,40)
 
     # Comandos
-    tecla = pygame.key.get_pressed()
     if tecla[pygame.K_RIGHT] and policia_x < 400:
         policia_x += 2
     if tecla[pygame.K_LEFT] and policia_x > 102.5:
